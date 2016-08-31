1042ce479cb7c505ad04b29883106bd3b740be1c
Andrey Nering <andrey.nering@gmail.com>
2016-08-31 08:45:13 -0300

    README and documentation

--- README.md
+++ README.md
@@ -1,3 +1,89 @@
 # Gormigrate
 
-Gormigrate is a migration helper for Gorm.
+Gormigrate is a migration helper for [Gorm][http://jinzhu.me/gorm/].
+Gorm already have useful [migrate functions][gormmigrate], just misses
+proper schema versioning and rollback cababilities.
+
+## Supported databases
+
+It supports the databases [Gorm supports][gormdatabases]:
+
+- PostgreSQL
+- MySQL
+- SQLite
+- Microsoft SQL Server
+
+## Installing
+
+```bash
+go get -u gopkg.in/gormigrate.v1
+```
+
+## Usage
+
+```go
+package main
+
+import (
+	"log"
+
+	"gopkg.in/gormigrate.v1"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Person struct {
+	gorm.Model
+	Name string
+}
+
+type Pet struct {
+	gorm.Model
+	Name     string
+	PersonID int
+}
+
+func main() {
+	db, err := gorm.Open("sqlite3", "mydb.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.DB().Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	db.LogMode(true)
+
+	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+		{
+			ID: "201608301400",
+			Migrate: func(tx *gorm.DB) error {
+				return tx.AutoMigrate(&Person{}).Error
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return tx.DropTable("people").Error
+			},
+		},
+		{
+			ID: "201608301430",
+			Migrate: func(tx *gorm.DB) error {
+				return tx.AutoMigrate(&Pet{}).Error
+			},
+			Rollback: func(tx *gorm.DB) error {
+				return tx.DropTable("pets").Error
+			},
+		},
+	})
+
+    err = m.Migrate()
+    if err == nil {
+		log.Printf("Migration did run successfully")
+    } else {
+		log.Printf("Could not migrate: %v", err)
+    }
+}
+```
+
+[gorm]: http://jinzhu.me/gorm/
+[gormmigrate]: http://jinzhu.me/gorm/database.html#migration
+[gormdatabases]: http://jinzhu.me/gorm/database.html#connecting-to-a-database
--- gormigrate.go
+++ gormigrate.go
@@ -1,3 +1,70 @@
+// Gormigrate is a migration helper for Gorm (http://jinzhu.me/gorm/).
+// Gorm already have useful migrate functions
+// (http://jinzhu.me/gorm/database.html#migration), just misses
+// proper schema versioning and rollback cababilities.
+//
+// Example:
+//
+//     package main
+//
+//     import (
+// 	       "log"
+//
+// 	       "github.com/go-gormigrate/gormigrate"
+// 	       "github.com/jinzhu/gorm"
+// 	       _ "github.com/jinzhu/gorm/dialects/sqlite"
+//     )
+//
+//     type Person struct {
+// 	        gorm.Model
+//          Name string
+//     }
+//
+//     type Pet struct {
+// 	        gorm.Model
+// 	        Name     string
+// 	        PersonID int
+//     }
+//
+//     func main() {
+// 	        db, err := gorm.Open("sqlite3", "mydb.sqlite3")
+// 	        if err != nil {
+// 		        log.Fatal(err)
+// 	        }
+// 	        if err = db.DB().Ping(); err != nil {
+// 	    	    log.Fatal(err)
+//         	}
+//
+// 	        db.LogMode(true)
+//
+//         	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+// 	        	{
+// 	        		ID: "201608301400",
+// 	        		Migrate: func(tx *gorm.DB) error {
+// 			        	return tx.AutoMigrate(&Person{}).Error
+//         			},
+// 		        	Rollback: func(tx *gorm.DB) error {
+// 				        return tx.DropTable("people").Error
+// 			        },
+//         		},
+// 	        	{
+// 		        	ID: "201608301430",
+// 		        	Migrate: func(tx *gorm.DB) error {
+// 			        	return tx.AutoMigrate(&Pet{}).Error
+// 		        	},
+// 			        Rollback: func(tx *gorm.DB) error {
+// 				        return tx.DropTable("pets").Error
+// 		        	},
+// 	        	},
+// 	        })
+//
+// 	        err = m.Migrate()
+//         	if err == nil {
+//         		log.Printf("Migration did run successfully")
+//         	} else {
+// 	        	log.Printf("Could not migrate: %v", err)
+//         	}
+//     }
 package gormigrate
 
 import (
@@ -10,15 +77,24 @@ import (
 type MigrateFunc func(*gorm.DB) error
 type RollbackFunc func(*gorm.DB) error
 
+// Options define options for all migrations.
 type Options struct {
-	TableName      string
-	IDColumnName   string
+	// TableName is the migration table.
+	TableName string
+	// IDColumnName is the name of column where the migration id will be stored.
+	IDColumnName string
+	// UseTransaction makes Gormigrate execute migrations inside a single transaction.
+	// Keep in mind that not all databases support DDL commands inside transactions.
 	UseTransaction bool
 }
 
+// Migrate represents a database migration (a modification to be made on the database).
 type Migration struct {
-	ID       string
-	Migrate  MigrateFunc
+	// ID is the migration identifier. Usually a timestamp like "201601021504".
+	ID string
+	// Migrate is a function that will br executed while running this migration.
+	Migrate MigrateFunc
+	// Rollback will be executed on rollback. Can be nil.
 	Rollback RollbackFunc
 }
 
@@ -29,15 +105,18 @@ type Gormigrate struct {
 }
 
 var (
+	// DefaultOptions can be used if you don't want to think about options.
 	DefaultOptions = &Options{
-		TableName:    "migration",
-		IDColumnName: "id",
+		TableName:      "migration",
+		IDColumnName:   "id",
+		UseTransaction: false,
 	}
 
 	ErrRollbackImpossible = errors.New("It's impossible to rollback this migration")
 	ErrNoMigrationDefined = errors.New("No migration defined")
 )
 
+// New returns a new Gormigrate.
 func New(db *gorm.DB, options *Options, migrations []*Migration) *Gormigrate {
 	return &Gormigrate{
 		db:         db,
@@ -68,6 +147,7 @@ func (g *Gormigrate) createMigrationTableIfNotExists() error {
 	return nil
 }
 
+// Migrate executes all migrations that did not run yet.
 func (g *Gormigrate) Migrate() error {
 	if err := g.db.DB().Ping(); err != nil {
 		return err
@@ -106,6 +186,7 @@ func (g *Gormigrate) Migrate() error {
 	return nil
 }
 
+// RollbackMigration undo a migration.
 func (g *Gormigrate) RollbackMigration(m *Migration) error {
 	if m.Rollback == nil {
 		return ErrRollbackImpossible
@@ -135,6 +216,7 @@ func (g *Gormigrate) RollbackMigration(m *Migration) error {
 	return nil
 }
 
+// RollbackLast undo the last migration
 func (g *Gormigrate) RollbackLast() error {
 	if len(g.migrations) == 0 {
 		return ErrNoMigrationDefined
