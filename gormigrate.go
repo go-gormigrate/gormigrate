bf45938863901f409fd867665a6dfed0cd460b48
Andrey Nering <andrey.nering@gmail.com>
2016-09-10 14:09:14 -0300

    Fix documentation indentation

--- gormigrate.go
+++ gormigrate.go
@@ -5,66 +5,66 @@
 //
 // Example:
 //
-//     package main
+//  package main
 //
-//     import (
-// 	       "log"
+//  import (
+//      "log"
 //
-// 	       "github.com/go-gormigrate/gormigrate"
-// 	       "github.com/jinzhu/gorm"
-// 	       _ "github.com/jinzhu/gorm/dialects/sqlite"
-//     )
+//      "github.com/go-gormigrate/gormigrate"
+//      "github.com/jinzhu/gorm"
+//      _ "github.com/jinzhu/gorm/dialects/sqlite"
+//  )
 //
-//     type Person struct {
-// 	        gorm.Model
-//          Name string
-//     }
+//  type Person struct {
+//      gorm.Model
+//      Name string
+//  }
 //
-//     type Pet struct {
-// 	        gorm.Model
-// 	        Name     string
-// 	        PersonID int
-//     }
+//  type Pet struct {
+//      gorm.Model
+//      Name     string
+//      PersonID int
+//  }
 //
-//     func main() {
-// 	        db, err := gorm.Open("sqlite3", "mydb.sqlite3")
-// 	        if err != nil {
-// 		        log.Fatal(err)
-// 	        }
-// 	        if err = db.DB().Ping(); err != nil {
-// 	    	    log.Fatal(err)
-//         	}
+//  func main() {
+//      db, err := gorm.Open("sqlite3", "mydb.sqlite3")
+//      if err != nil {
+//          log.Fatal(err)
+//      }
+//      if err = db.DB().Ping(); err != nil {
+//          log.Fatal(err)
+//      }
 //
-// 	        db.LogMode(true)
+//      db.LogMode(true)
 //
-//         	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
-// 	        	{
-// 	        		ID: "201608301400",
-// 	        		Migrate: func(tx *gorm.DB) error {
-// 			        	return tx.AutoMigrate(&Person{}).Error
-//         			},
-// 		        	Rollback: func(tx *gorm.DB) error {
-// 				        return tx.DropTable("people").Error
-// 			        },
-//         		},
-// 	        	{
-// 		        	ID: "201608301430",
-// 		        	Migrate: func(tx *gorm.DB) error {
-// 			        	return tx.AutoMigrate(&Pet{}).Error
-// 		        	},
-// 			        Rollback: func(tx *gorm.DB) error {
-// 				        return tx.DropTable("pets").Error
-// 		        	},
-// 	        	},
-// 	        })
+//      m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+//          {
+//              ID: "201608301400",
+//              Migrate: func(tx *gorm.DB) error {
+//                  return tx.AutoMigrate(&Person{}).Error
+//              },
+//              Rollback: func(tx *gorm.DB) error {
+//                  return tx.DropTable("people").Error
+//              },
+//          },
+//          {
+//              ID: "201608301430",
+//              Migrate: func(tx *gorm.DB) error {
+//                  return tx.AutoMigrate(&Pet{}).Error
+//              },
+//              Rollback: func(tx *gorm.DB) error {
+//                  return tx.DropTable("pets").Error
+//              },
+//          },
+//      })
 //
-// 	        err = m.Migrate()
-//         	if err == nil {
-//         		log.Printf("Migration did run successfully")
-//         	} else {
-// 	        	log.Printf("Could not migrate: %v", err)
-//         	}
-//     }
+//      err = m.Migrate()
+//      if err == nil {
+//          log.Printf("Migration did run successfully")
+//      } else {
+//          log.Printf("Could not migrate: %v", err)
+//      }
+//  }
 package gormigrate
 
 import (
